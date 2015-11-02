3df8797f5f06547c823447df2f488a64a6560fd9
bandienkhamgalan <bandi.enkhamgalan@gmail.com>
2015-11-02 22:09:03 +0000

    setup Distelli, rename project to 'app', add 'requirements.txt' for virtualenv

--- app/COMP204P/settings.py
+++ app/COMP204P/settings.py
@@ -15,12 +15,10 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 from passwords import GITHUB_SECRET_KEY
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = GITHUB_SECRET_KEY
 
 DEBUG = True

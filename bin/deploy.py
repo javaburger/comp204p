1e5899147d72d5fe6508118f60c1b3139536e739
bandienkhamgalan <bandi.enkhamgalan@gmail.com>
2015-11-03 15:12:37 +0000

    fix deploy issues

--- bin/deploy.py
+++ bin/deploy.py
@@ -6,10 +6,10 @@ env.user = 'localuser'
 env.hosts = ['localhost:1234']
 
 def deploy():
-	# run('git clone https://github.com/javaburger/comp204p.git')
+	run('git clone https://github.com/javaburger/comp204p.git || true')
 	with cd('~/comp204p'):
-		run('sudo killall gunicorn')
-		run('git reset --hard')
+		run('sudo killall gunicorn || true')
+		run('git reset --hard || true')
 		# run('git clean -f -d')
 		# run('git clean -f -x -d')
 	with cd('~/comp204p/app'):

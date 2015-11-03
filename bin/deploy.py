dadd9064ab39d3d17505425d098c2fcc0b5c1a59
bandienkhamgalan <bandi.enkhamgalan@gmail.com>
2015-11-03 13:10:38 +0000

    potentially fix deploy scripts

--- bin/deploy.py
+++ bin/deploy.py
@@ -6,11 +6,13 @@ env.user = 'localuser'
 env.hosts = ['localhost:1234']
 
 def deploy():
-	run('git clone https://github.com/javaburger/comp204p.git')
-	cd('~/comp204p')
-	run('sudo killall gunicorn')
-	run('git pull origin master')
-	cd('~/comp204p/app')
-	run('python manage.py syncdb')
-	cd('~/comp204p/bin')
-	run('sudo ./gunicorn_start.bash')
+	# run('git clone https://github.com/javaburger/comp204p.git')
+	with cd('~/comp204p'):
+		run('sudo killall gunicorn')
+		run('git reset --hard')
+		# run('git clean -f -d')
+		# run('git clean -f -x -d')
+	with cd('~/comp204p/app'):
+		run('python manage.py syncdb')
+	with cd('~/comp204p/bin'):
+		run('sudo ./gunicorn_start.bash')
--- bin/gunicorn_start.bash
+++ bin/gunicorn_start.bash
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 NAME="ToDoListApp"
-DJANGODIR=~/comp204p/app
 BIND_ADDRESS=0.0.0.0:80
 NUM_WORKERS=5
 DJANGO_WSGI_MODULE=COMP204P.wsgi
 
 echo "Starting $NAME as `whoami`"
 
-cd $DJANGODIR
+cd ~/comp204p/app
 
 exec gunicorn ${DJANGO_WSGI_MODULE}:application --name $NAME --workers $NUM_WORKERS --bind=$BIND_ADDRESS

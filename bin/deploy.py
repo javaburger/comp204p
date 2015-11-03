c92a0b57313fb33536285ef0f99e0610982220e2
bandienkhamgalan <bandi.enkhamgalan@gmail.com>
2015-11-03 23:55:14 +0000

    fix deploy scripts

--- bin/deploy.py
+++ bin/deploy.py
@@ -8,12 +8,10 @@ env.hosts = ['localhost:1234']
 def deploy():
 	run('git clone https://github.com/javaburger/comp204p.git || true')
 	with cd('~/comp204p'):
-		run('sudo killall gunicorn || true')
+		run('sudo systemctl stop gunicorn')
 		run('git reset --hard || true')
 		run('git pull origin master || true')
-		# run('git clean -f -d')
-		# run('git clean -f -x -d')
 	with cd('~/comp204p/app'):
 		run('python manage.py syncdb')
 	with cd('~/comp204p/app'):
-		run('sudo ../bin/gunicorn_start.bash')
+		run('sudo systemctl start gunicorn')

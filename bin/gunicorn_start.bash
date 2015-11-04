b48ed80ad6b738d7c47e4628aa4195d0498c1e3e
bandienkhamgalan <bandi.enkhamgalan@gmail.com>
2015-11-04 22:47:24 +0000

    drop gunicorn as external web server, bind gunicorn to unix sock

--- bin/gunicorn_start.bash
+++ bin/gunicorn_start.bash
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 NAME="ToDoListApp"
-BIND_ADDRESS=0.0.0.0:80
+BIND_ADDRESS=unix:/home/localuser/comp204p/app.sock
 NUM_WORKERS=5
 DJANGO_WSGI_MODULE=COMP204P.wsgi
 
 echo "Starting $NAME as `whoami`"
 
-exec sudo gunicorn ${DJANGO_WSGI_MODULE}:application --pythonpath '/home/localuser/comp204p/app' --name $NAME --workers $NUM_WORKERS --bind=$BIND_ADDRESS
+exec gunicorn ${DJANGO_WSGI_MODULE}:application --pythonpath '/home/localuser/comp204p/app' --name $NAME --workers $NUM_WORKERS --bind=$BIND_ADDRESS

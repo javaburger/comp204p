50b75c0f965c124138b36d6d251dae3e5ca20150
bandienkhamgalan <bandi.enkhamgalan@gmail.com>
2015-11-04 20:40:58 +0000

    fix bash syntax error

--- bin/restartAndPull.bash
+++ bin/restartAndPull.bash
@@ -10,4 +10,4 @@ git pull origin master || true;
 cd /home/localuser/comp204p/app;
 python manage.py syncdb;
 sudo systemctl start gunicorn;
-echo restarted at `date "+%Y/%m/%d %H:%M:%S"`\\n >> /home/localuser/comp204p/logs.txt
+echo restarted at `date "+%Y/%m/%d %H:%M:%S"`\\n >> /home/localuser/comp204p/logs.txt;

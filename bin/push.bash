1dc4402cea71dc6581668274b5db054ae1096154
bandienkhamgalan <bandi.enkhamgalan@gmail.com>
2015-11-04 00:16:04 +0000

    fix push script

--- bin/push.bash
+++ bin/push.bash
@@ -1,18 +1,24 @@
 #!/bin/bash
 
-LOCAL_SSH_PORT=1234
-VM_HOSTNAME=studbenkhamg-p.cs.ucl.ac.uk
-TUNNEL_USERNAME=benkhamg
-TUNNEL_HOSTNAME=newgate.cs.ucl.ac.uk
+LOCAL_SSH_PORT=1234;
+VM_HOSTNAME=studbenkhamg-p.cs.ucl.ac.uk;
+TUNNEL_USERNAME=benkhamg;
+TUNNEL_HOSTNAME=newgate.cs.ucl.ac.uk;
 
 # Kill existing SSH tunnel
-killall ssh
+PID=$(lsof -i tcp:${LOCAL_SSH_PORT} -t);
+if [[ -n "$PID" ]];
+	then kill -9 $PID;
+fi;
 
 # Setup SSH tunnel
-ssh -N -L${LOCAL_SSH_PORT}:${VM_HOSTNAME}:22 ${TUNNEL_USERNAME}@${TUNNEL_HOSTNAME} -f
+ssh -N -L${LOCAL_SSH_PORT}:${VM_HOSTNAME}:22 ${TUNNEL_USERNAME}@${TUNNEL_HOSTNAME} -f;
 
 # Run Fabric script
-fab -f ~/www/comp204p/bin/deploy.py deploy
+fab -f ~/www/comp204p/bin/deploy.py deploy;
 
 # Close SSH tunnel
-killall ssh
+PID=$(lsof -i tcp:${LOCAL_SSH_PORT} -t);
+if [[ -n "$PID" ]];
+	then kill -9 $PID;
+fi;

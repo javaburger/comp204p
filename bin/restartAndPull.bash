1e63a0127b7edc9a40625ab803635ca4bca820f6
bandienkhamgalan <bandi.enkhamgalan@gmail.com>
2015-11-04 12:30:59 +0000

    fix restartandpull bash script

--- bin/restartAndPull.bash
+++ bin/restartAndPull.bash
@@ -7,6 +7,7 @@ sudo systemctl stop gunicorn;
 sudo pip install -r requirements.txt;
 git reset --hard || true;
 git pull origin master || true;
+cd /home/localuser/comp204p/app;
 python manage.py syncdb;
 sudo systemctl start gunicorn;
 echo restarted at `date "+%Y/%m/%d %H:%M:%S"`\\n >> /home/localuser/comp204p/logs.txt

8d048db77a0984e61b6ae367e4cc2011b7a4f42e
bandienkhamgalan <bandi.enkhamgalan@gmail.com>
2015-11-04 20:48:14 +0000

    add testing code for autodeploy

--- bin/restartAndPull.bash
+++ bin/restartAndPull.bash
@@ -1,11 +1,14 @@
 #!/bin/bash
-
+echo starting pull and restart at `date "+%Y/%m/%d %H:%M:%S"`\\n >> /home/localuser/comp204p/logs.txt;
 cd /home/localuser;
+echo cloning at `date "+%Y/%m/%d %H:%M:%S"`\\n >> /home/localuser/comp204p/logs.txt;
 git clone https://github.com/javaburger/comp204p.git || true;
 cd /home/localuser/comp204p;
+echo stopping gunicorn `date "+%Y/%m/%d %H:%M:%S"`\\n >> /home/localuser/comp204p/logs.txt;
 sudo systemctl stop gunicorn;
 sudo pip install -r requirements.txt;
 git reset --hard || true;
+echo pulling changes `date "+%Y/%m/%d %H:%M:%S"`\\n >> /home/localuser/comp204p/logs.txt;
 git pull origin master || true;
 cd /home/localuser/comp204p/app;
 python manage.py syncdb;

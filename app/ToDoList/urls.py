815fb68e4476a56b7503d0982b12ad3a11cee19f
bandienkhamgalan <bandi.enkhamgalan@gmail.com>
2015-11-05 13:03:59 +0000

    potentially fix import issues

--- app/ToDoList/urls.py
+++ app/ToDoList/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 
 from . import views
-import COMP204P.config
+from COMP204P import config
 
 urlpatterns = [   
 	url(r'^$', views.index, name='index'),
--- app/ToDoList/views.py
+++ app/ToDoList/views.py
@@ -16,7 +16,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
-import COMP204P.config
+from COMP204P import config
 
 import hmac
 import hashlib

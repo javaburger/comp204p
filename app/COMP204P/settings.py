ecd93c6d976e40d29039c08bd0e025a6f84b76da
bandienkhamgalan <bandi.enkhamgalan@gmail.com>
2015-11-05 11:17:58 +0000

    configure server to set debug mode appropriately by loading env variable

--- app/COMP204P/settings.py
+++ app/COMP204P/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 from passwords import GITHUB_SECRET_KEY
 SECRET_KEY = GITHUB_SECRET_KEY
 
-DEBUG = False
+DEBUG = os.environ['DJANGO_DEBUG_MODE']
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'studvm28-p.cs.ucl.ac.uk']
 

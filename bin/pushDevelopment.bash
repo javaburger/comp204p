bd3352f0fa1e2c24d582e45bb587e19cb923d0fa
bandienkhamgalan <bandi.enkhamgalan@gmail.com>
2015-11-05 13:13:22 +0000

    update manual deploy scripts

---
+++ bin/pushDevelopment.bash
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+LOCAL_SSH_PORT=1234;
+VM_HOSTNAME=studsdhall-p.cs.ucl.ac.uk;
+TUNNEL_USERNAME=sdhall;
+TUNNEL_HOSTNAME=newgate.cs.ucl.ac.uk;
+
+# Kill existing SSH tunnel
+PID=$(lsof -i tcp:${LOCAL_SSH_PORT} -t);
+if [[ -n "$PID" ]];
+	then kill -9 $PID;
+fi;
+
+# Setup SSH tunnel
+ssh -N -L${LOCAL_SSH_PORT}:${VM_HOSTNAME}:22 ${TUNNEL_USERNAME}@${TUNNEL_HOSTNAME} -f;
+
+# Run Fabric script
+fab -f ~/www/comp204p/bin/deploy.py deploy;
+
+# Close SSH tunnel
+PID=$(lsof -i tcp:${LOCAL_SSH_PORT} -t);
+if [[ -n "$PID" ]];
+	then kill -9 $PID;
+fi;

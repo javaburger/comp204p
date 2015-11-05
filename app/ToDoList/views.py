02b70162af2ad14d919da55acd6a7f97a2b1ba79
bandienkhamgalan <bandi.enkhamgalan@gmail.com>
2015-11-05 12:57:47 +0000

    potentially fix import issues

--- app/ToDoList/views.py
+++ app/ToDoList/views.py
@@ -16,7 +16,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
-from .. import config
+from ..COMP204P import config
 
 import hmac
 import hashlib
